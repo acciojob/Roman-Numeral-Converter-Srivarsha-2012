01de718e0cf970162e77af1743db4a95909098f0
srivarsha <algotesrivarsha@gmail.com>
2026-04-02 21:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
 function toRoman(num) {
-    // Handle 0 (most platforms expect empty string)
+    // Handle 0
     if (num === 0) return "";
 
-    // Roman numeral mapping
-    const values = [
-        1000, 900, 500, 400,
-        100, 90, 50, 40,
-        10, 9, 5, 4,
-        1
-    ];
-
-    const symbols = [
-        "M", "CM", "D", "CD",
-        "C", "XC", "L", "XL",
-        "X", "IX", "V", "IV",
-        "I"
+    const map = [
+        [1000, "M"], [900, "CM"], [500, "D"], [400, "CD"],
+        [100, "C"], [90, "XC"], [50, "L"], [40, "XL"],
+        [10, "X"], [9, "IX"], [5, "V"], [4, "IV"],
+        [1, "I"]
     ];
 
     let result = "";
 
-    for (let i = 0; i < values.length; i++) {
-        while (num >= values[i]) {
-            result += symbols[i];
-            num -= values[i];
+    for (let [value, symbol] of map) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
         }
     }
 
